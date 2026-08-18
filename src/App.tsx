cad7db345eff756fd1c9b7dcbe4eb4203e9e8dfb
Adnan Aydin <xanadu.ch@gmail.com>
2026-08-18 03:12:49 +0200

    refactor: restore original ProjectsSection component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Header } from './components/Header';
 import { HeroSection } from './components/HeroSection';
 import { AboutSection } from './components/AboutSection';
-import { ProjectsSection } from './components/test';
+import { ProjectsSection } from './components/ProjectsSection';
 
 import { SkillsSection } from './components/SkillsSection';
 import { ExperienceSection } from './components/ExperienceSection';
